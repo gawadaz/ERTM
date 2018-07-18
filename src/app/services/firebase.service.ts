eaf88e4f89d10a252cb176d22d4150f49202b290
gawadaz <gawad.azzam@gmail.com>
2018-07-18 08:46:16 +0300

    adding electors table

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -8,11 +8,11 @@ import { map } from 'rxjs/operators';
 })
 export class FirebaseService {
 
-  ertmRef: AngularFireList<any>;
+  ertmRef: AngularFireList<{}>;
   electors: Observable<any[]>;
 
-  constructor(db: AngularFireDatabase) {
-    this.ertmRef = db.list('codes', ref => ref.orderByKey().startAt('0').limitToFirst(20));
+  constructor(private db: AngularFireDatabase) {
+  /*  this.ertmRef = db.list('codes', ref => ref.orderByKey().startAt('0').limitToFirst(20));
     console.log('list: ' + JSON.stringify(this.ertmRef));
     this.electors = this.ertmRef.snapshotChanges().pipe(
       map(items => {
@@ -22,10 +22,17 @@ export class FirebaseService {
    // console.log(this.electors);
     this.electors.forEach(elector => {
       // console.log('elector: ' + JSON.stringify(elector));
-    });
+    });*/
    }
 
-   getElectorsList() {
-     return this.electors;
+   getElectorsList(numberOfItems: number, startKey?: string) {
+    this.ertmRef = this.db.list('codes', ref => ref.orderByKey().startAt(startKey).limitToFirst(numberOfItems + 1));
+    // this.ertmRef = this.db.list('codes', ref => ref.orderByKey().equalTo('32690893').limitToFirst(numberOfItems + 1));
+      this.electors = this.ertmRef.snapshotChanges().pipe(
+        map(items => {
+          return items.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+        })
+      );
+      return this.electors;
    }
 }
--- src/app/test-table/test-table.component.ts
+++ src/app/test-table/test-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
 import { Subscription } from 'rxjs';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-test-table',
@@ -9,20 +10,48 @@ import { Subscription } from 'rxjs';
 })
 export class TestTableComponent implements OnInit, OnDestroy {
 
+
   electorsSubscription: Subscription;
   electors: any[];
+  numberOfItems = 20;
+  nextKey: any;
+  prevKeys: any[] = [];
+  searchID: string;
 
   constructor(private fbService: FirebaseService) { }
 
   ngOnInit() {
-    this.electorsSubscription = this.fbService.getElectorsList().subscribe( (electors) => {
+    this.getElectorsList();
+    /*this.electorsSubscription = this.fbService.getElectorsList(this.numberOfItems, '0').subscribe( (electors) => {
       this.electors = electors;
-      // this.electors = this.electors.slice(0, 10);
-      // console.log('electors: ' + JSON.stringify(electors));
+     } );*/
+  }
+
+  getElectorsList(key?) {
+    if (this.electorsSubscription) { this.electorsSubscription.unsubscribe(); }
+
+    if (!key) { key = '0'; }
+
+    this.electorsSubscription = this.fbService.getElectorsList(this.numberOfItems, key).subscribe( (electors) => {
+      this.electors = _.slice(electors, 0, this.numberOfItems);
+      this.nextKey = _.get(electors[this.numberOfItems], 'key');
      } );
+  }
+
+  onNext() {
+    this.prevKeys.push(_.first(this.electors)['key']);
+    this.getElectorsList(this.nextKey);
+  }
+
+  onPrev() {
+    const prevKey = _.last(this.prevKeys);
+    this.prevKeys = _.dropRight(this.prevKeys);
+
+    this.getElectorsList(prevKey);
   }
 
   ngOnDestroy(): void {
     this.electorsSubscription.unsubscribe();
   }
+
 }
